c37b5e0e72facdea23f1e17797e428128e29d84d
Пользователь <01txxt10@gmail.com>
2023-08-02 14:22:25 +0700

    Размечает и стилизует блок с ближайшими турами

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -10,6 +10,29 @@ function initSliders() {
       clickable: true,
     },
   });
+
+  const nextToursSlider = new Swiper('.next-tours__slider', {
+    direction: 'horizontal',
+    slidesPerView: 3,
+    spaceBetween: 30,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 18,
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      }
+    },
+    navigation: {
+      nextEl: '.next-tours__slider-next',
+      prevEl: '.next-tours__slider-prev',
+    },
+  });
 }
 
 export default initSliders;
